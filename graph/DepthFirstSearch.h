b25a078aaddbb0170424714879aaea0aa0ac192e
XMLee <56443343+Banner-lab@users.noreply.github.com>
2021-04-12 18:14:03 +0800

    Update DepthFirstSearch.h
    增加在无向连通图中找出两点之间 所有路径函数

--- graph/DepthFirstSearch.h
+++ graph/DepthFirstSearch.h
@@ -10,13 +10,14 @@
 class DepthFirstSearch{
     private:
         vector<bool> marked;
+        vector<vector<int>> allways;
         int count;
+
     public:
         DepthFirstSearch(Graph* g,int s){
             marked.resize(g->V(),false);
             count=0;
-
-            (g,0);
+            dfs(g,s);
         }
 
         void dfs(Graph* g,int s){
@@ -36,6 +37,35 @@ class DepthFirstSearch{
         bool Marked(int v){
             return marked[v];
         }
+
+        //find all the way from s to cur
+        void findAllways(Graph* g,int v,int cur,vector<int>& temp){
+            marked[v]=true;
+            if(v==cur){
+                allways.push_back(temp);
+                marked[v]=false;
+                return ;
+            }
+
+            for(int w:g->Adj(v)){
+                if(!marked[w]){
+                    temp.push_back(w);
+                    findAllways(g,w,cur,temp);
+                    temp.pop_back();
+                    marked[w]=false;
+                }
+            }
+        }
+
+        vector<vector<int>> DFS(Graph* g,int s,int e){
+            for(int i=0;i<marked.size();i++)
+                marked[i]=false;
+            vector<int> temp;
+
+            findAllways(g,s,e,temp);
+            return allways;
+        }
+        //
 };
 
 #endif //KAOYAN_DEPTHFIRSTSEARCH_H
